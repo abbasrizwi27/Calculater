d124f50ed76f5460dfc00b1dd1889f413bafb4d1
Aqsa Trust <abbasmohd753@gmail.com>
2021-08-05 19:52:13 +0530

    Second update

--- src/com/company/Calculater.java
+++ src/com/company/Calculater.java
@@ -1,10 +1,18 @@
 package com.company;
 
 public class Calculater {
-    int Add(String numbers){
+    int Add(String numbers)
+    {
+        int sum=0;
         if(numbers.isEmpty()){
             return 0;
         }
-        
+        String[] nbr=numbers.split(",");
+        for(String i:nbr){
+            sum += Integer.parseInt(i);
+
+        }
+        return sum;
+
     }
 }
